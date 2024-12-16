beb5df53dbc1fbdd1390f631396a816c28c66465
BimlaJain <vj13798@gmail.com>
2024-12-16 10:42:30 +0530

    change container size

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ module.exports = {
           sm: "640px",
           md: "768px",
           lg: "1024px",
-          xl: "1200px",
+          xl: "1140px",
         },
       },
       fontSize: {
