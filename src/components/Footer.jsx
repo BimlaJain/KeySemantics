4ee4cd7c244101dc63bb11891217921957f30cba
BimlaJain <vj13798@gmail.com>
2024-12-16 14:25:52 +0530

    change footer in small screen

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='bg-darkblue py-6 -mt-1'>
-          <div className="container flex justify-between">
-              <p className='text-voilet text-xs font-normal'>©2024 KeySemantics GmbH | Ramsenburgweg 24, 9100 Herisau, Switzerland</p>
-              <p className='text-voilet text-xs font-normal cursor-pointer'>info@keysemantics.ch</p>
+          <div className="container sm:flex justify-between">
+              <p className='text-voilet text-xs font-normal max-sm:text-center'>©2024 KeySemantics GmbH | Ramsenburgweg 24, 9100 Herisau, Switzerland</p>
+              <p className='text-voilet text-xs font-normal cursor-pointer max-sm:pt-2 max-sm:text-center'>info@keysemantics.ch</p>
           </div>
     </div>
   )
