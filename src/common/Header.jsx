6194980b6d20e3a6d063ff6c0cc4196499837adc
BimlaJain <vj13798@gmail.com>
2024-12-16 15:36:21 +0530

    change logo place

--- src/common/Header.jsx
+++ src/common/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import CommonButton from './CommonButton'
+import { Link } from 'react-router-dom'
 const Header = () => {
     return (      
       <div className='flex justify-between md:pt-[47px] pt-7'>
-              <img className='lg:w-[248px] md:w-[240px] w-[160px] cursor-pointer' src="./assets/images/webp/logo.webp" alt="logo" />
+          <img className='lg:w-[248px] md:w-[240px] w-[160px] cursor-pointer' src="./assets/images/webp/logo.webp" alt="logo" />
         <CommonButton classList="!py-2 !px-3 !max-sm:text-sm" NavButton="Request a demo"/>
     </div>
   )
